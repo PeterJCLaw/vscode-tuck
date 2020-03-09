73401de9ab0dadcfb73531029eaefd3133af6b98
Peter Law <PeterJCLaw@gmail.com>
2020-03-09 12:02:04 +0000

    Assume all dependencies are explicitly present

--- pythonFiles/install-requirements.sh
+++ pythonFiles/install-requirements.sh
@@ -7,9 +7,11 @@ python3 -m venv venv
 
 pip install --upgrade pip
 
-# Install the depdencies into `lib`, requiring that we're platform agnostic
+# Install the depdencies into `lib`, requiring that we're platform agnostic.
+# Note: we assume that all dependencies are explicitly present in `requirements.txt`.
 pip install \
     --requirement tuck/requirements.txt \
     --target lib \
     --implementation py \
+    --no-deps \
     --upgrade
